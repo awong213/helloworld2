ae9a129e6a9d83c096e800c7fdff88e7206090f1
coolwong <coolwong@terpmail.umd.edu>
2026-09-09 23:07:30 -0400

    Added new route for hello HTML page

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/')
 def hello_world():  # put application's code here
+    return 'Hello World from Amber Wong! I am adding my first code change.'
+
+@app.route('/hello')
+def hello():
     return render_template('hello.html')
 
 
