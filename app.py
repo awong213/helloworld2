79950efcd687a7372b4d2d317f2f2e12d4525fbb
coolwong <coolwong@terpmail.umd.edu>
2026-09-23 14:26:48 -0400

    changed introductory message and color of headings

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World from Amber Wong! I am adding my first code change.'
+
 
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run()
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
